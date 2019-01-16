fc333ff952893b08487ba814fc4ae1fc2dbabe8c
Brian Dooley <brian.dooley@feedhenry.com>
2019-01-16 09:27:28 +0000

    added endpoint

--- application.js
+++ application.js
@@ -1,22 +1,23 @@
-var express = require('express'),
-    fs = require('fs'),
-    app = express();
-//    eps = require('ejs'),
-//    morgan = require('morgan');
-
+var cors = require('cors');
+var express = require('express');
 var app = express();
+
+app.use(cors());
 
 var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.use(express.static('public'));
+app.use('/health', require('./lib/health.js')());
 
 // app is running!
 console.log('running at ' + new Date());
+
 app.get('/', function(req, res) {
-    res.send('mailforce pinged at ' + new Date());
+    res.send('pinged at ' + new Date());
     console.log('Pinged at ' + new Date());
 });
 
+var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 app.listen(8080, ip);
 
 module.exports = app;
